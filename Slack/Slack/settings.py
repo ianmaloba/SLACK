c310ebae3588bd14fde3fc49e2d73f377ca5ee58
Ian Maloba <ianmalobamwakha@gmail.com>
2023-11-17 17:46:29 +0300

    Set up MySQL Database, Created  SlackDb Database

--- Slack/Slack/settings.py
+++ Slack/Slack/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Chat',
 ]
 
 MIDDLEWARE = [
@@ -73,13 +74,29 @@ WSGI_APPLICATION = 'Slack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+"""
+# I'm using mysql database instead! (updated below)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SlackDb',
+        'USER': 'root',
+        'PASSWORD': '@scientisT.1',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
