01cf250f5134e9c52d38bfbe9302bb679e08a8bd
Ian Maloba <ianmalobamwakha@gmail.com>
2023-11-18 04:33:46 +0300

    setting up django channels

--- Slack/Slack/settings.py
+++ Slack/Slack/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Chat',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -134,7 +136,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = 'Slack.asgi.application'
