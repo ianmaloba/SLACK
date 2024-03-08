41640ef441fa85da07f5fad17cb95dc0b4a19c88
Ian Maloba <ianmalobamwakha@gmail.com>
2024-03-08 06:46:06 +0300

    modified:   Slack/static/js/main.js

--- Slack/static/js/main.js
+++ Slack/static/js/main.js
@@ -104,11 +104,45 @@ const constraints = {
 
 const localVideo = document.querySelector('#local-video');
 
+const btnToggleAudio = document.querySelector('#btn-toggle-audio');
+const btnToggleVideo = document.querySelector('#btn-toggle-video');
+
 var userMedia = navigator.mediaDevices.getUserMedia(constraints)
     .then(stream => {
         localStream = stream;
         localVideo.srcObject = localStream;
         localVideo.muted = true;
+
+        var audioTracks = stream.getAudioTracks();
+        var videoTracks = stream.getVideoTracks();
+
+        audioTracks[0].enabled = true;
+        videoTracks[0].enabled = true;
+
+        btnToggleAudio.addEventListener('click', () => {
+            audioTracks[0].enabled = !audioTracks[0].enabled;
+
+            if(audioTracks[0].enabled){
+                btnToggleAudio.innerHTML = 'Audio Mute';
+
+                return;
+            }
+
+            btnToggleAudio.innerHTML = 'Audio Unmute';
+        });
+
+        btnToggleVideo.addEventListener('click', () => {
+            videoTracks[0].enabled = !videoTracks[0].enabled;
+
+            if(videoTracks[0].enabled){
+                btnToggleVideo.innerHTML = 'Video Off';
+
+                return;
+            }
+
+            btnToggleVideo.innerHTML = 'Video On';
+        });
+
     })
     .catch(error => {
         console.log('Error accessing media devices.', error);
